82835940251bc61d1518081f86bf3fa8c5398efe
Ankit <akumarg1216@gmail.com>
2022-01-12 13:36:24 +0530

    Updated till section-14 End

--- src/index.js
+++ src/index.js
@@ -13,19 +13,3 @@ app.use(taskRouter)
 app.listen(port, () => {
     console.log('Server is up on port ' + port)
 })
-
-const Task = require('./models/task')
-const User = require('./models/user')
-
-// const main = async() => {
-//     // const task = await Task.findById('61dd8fd72151bc6fd8be19ae')
-//     // await task.populate('owner').execPopulate()
-//     // console.log(task.owner)
-
-//     const user = await User.findById('61dd8fd32151bc6fd8be19ac')
-//     await user.populate('tasks').execPopulate()
-//     console.log(user.tasks);
-
-// }
-
-// main()
